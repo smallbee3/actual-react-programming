29a1860585adc3446694c5a439a25166ecda01b2
smallbee <smallbee3@gmail.com>
2019-12-20 01:01:40 +0900

    1.3.1| 'code 5-22' added to cra-test

--- 1-chapter/cra-test/src/App.js
+++ 1-chapter/cra-test/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [age, setAge] = useState(20);
+  const prevAgeRef = useRef(20);
+  useEffect(() => {
+    prevAgeRef.current = age;
+  }, [age]);
+  const prevAge = prevAgeRef.current;
+  const text = age === prevAge ? 'same' : age > prevAge ? 'order' : 'younger';
+  return React.createElement("div", null, React.createElement("p", null, `age ${age} is ${text} than age ${prevAge}`), React.createElement("button", {
+    onClick: () => {
+      const age = Math.floor(Math.random() * 50 + 1);
+      setAge(age);
+    }
+  }, "\uB098\uC774\uBCC0\uACBD"));
+} // const domContainer = document.querySelector('#react-root');
+// ReactDOM.render(React.createElement(Profile), domContainer);
+
 
 export default App;
---
+++ 1-chapter/cra-test/src/jsx/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useRef, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    const [age, setAge] = useState(20);
+    const prevAgeRef = useRef(20);
+    useEffect(
+        () => {
+            prevAgeRef.current = age;
+        },
+        [age],
+    );
+    const prevAge =prevAgeRef.current;
+    const text = age === prevAge ? 'same' : age > prevAge ? 'order' : 'younger';
+    return (
+        <div>
+            <p>{`age ${age} is ${text} than age ${prevAge}`}</p>
+            <button 
+                onClick={() => {
+                    const age = Math.floor(Math.random() * 50 + 1);
+                    setAge(age);
+                }}
+            >
+                나이변경
+            </button>
+        </div>
+    );
+}
+
+// const domContainer = document.querySelector('#react-root');
+// ReactDOM.render(React.createElement(Profile), domContainer);
+export default App;
