dcb728170a1f23b91cb23c869a806558284daf30
smallbee <smallbee3@gmail.com>
2019-12-19 21:06:27 +0900

    1.2.1| simple2.html tested according to suggestion in p.8

---
+++ 1-chapter/1-hello-world/simple2-2.js
@@ -0,0 +1,30 @@
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+  render() {
+    const text = this.state.liked ? '좋아요 취소' : '좋아요';
+    return React.createElement(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      text,
+      React.createElement(
+        'button',
+        { onClick: () => this.setState({ liked: true }) },
+        text,      
+      ),
+      React.createElement(
+        'button',
+        { onClick: () => this.setState({ liked: true }) },
+        text,      
+      )
+    );
+  }
+}
+
+ReactDOM.render(
+  React.createElement(LikeButton),
+  document.querySelector('#react-root'),
+);
+ 
