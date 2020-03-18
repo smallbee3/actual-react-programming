3bd51c36b52a3cf146e11f81a00b8687e9fdecc9
smallbee <smallbee3@gmail.com>
2020-03-18 13:03:34 +0900

    Practice 'UseEffect' hook

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -49,6 +49,67 @@ import { Component } from 'react';
 
 // 4-26 DataSet
 
+// class App extends Component {
+//   state = {
+//     selectedName: 'mike',
+//   };
+//   onClick = e => {
+//     const selectedName = e.currentTarget.dataset.name;
+//     this.setState({ selectedName });
+//   };
+//   render() {
+//     const { selectedName } = this.state;
+//     return (
+//       <div>
+//         <button onClick={this.onClick} data-name="mike">mike</button>
+//         <button onClick={this.onClick} data-name="john">john</button>
+//         {`selectedName is ${selectedName}`}
+//       </div>
+//     )
+//   }
+// }
+
+
+// 2020-03-18 UseEffect
+
+function Module() {
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+  useEffect(() => {
+    console.log('렌더링이 완료되었습니다!');
+    console.log({
+      name,
+      nickname
+    });
+  }, [name]);
+
+  const onChangeName = e => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = e => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b>
+          {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
 class App extends Component {
   state = {
     selectedName: 'mike',
@@ -64,6 +125,9 @@ class App extends Component {
         <button onClick={this.onClick} data-name="mike">mike</button>
         <button onClick={this.onClick} data-name="john">john</button>
         {`selectedName is ${selectedName}`}
+        <div>
+          <Module></Module>
+        </div>
       </div>
     )
   }
